231f405e084ebeb998139e81c5e11e191104bf14
Mridul <mit.ash.619@gmail.com>
2023-03-26 14:26:28 +0530

    update leetcode

--- week 8- divide and conquer/Lecture 6/leetcode 37.cpp
+++ week 8- divide and conquer/Lecture 6/leetcode 37.cpp
@@ -1,74 +1,67 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-bool isSafe(int row, int col, vector<vector<char>> &board, char value)
+class Solution
 {
-    int n = board.size();
-
-    for (int i = 0; i < n; i++)
+public:
+    bool isSafe(int row, int col, vector<vector<char>> &board, char value)
     {
-        // row check
-        if (board[row][i] == value)
-            return false;
+        int n = board.size();
 
-        // col check
-        if (board[i][col] == value)
-            return false;
-
-        // 3x3 box
-        if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value)
-            return false;
-    }
-    return true;
-}
-
-bool solve(vector<vector<char>> &board)
-{
-    int n = board.size();
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
+        for (int i = 0; i < n; i++)
         {
-            // check for empty cell
-            if (board[i][j] == '.')
-            {
-                // try to fill vales [1,9]
-                for (char val = '1'; val <= '9'; val++)
-                {
-                    // check for safety
-                    if (isSafe(i, j, board, val))
-                    {
-                        // insert
-                        board[i][j] = val;
+            // row check
+            if (board[row][i] == value)
+                return false;
 
-                        // reucusion
-                        bool restSolution = solve(board);
-                        if (restSolution == true)
-                            return true;
-                        else
+            // col check
+            if (board[i][col] == value)
+                return false;
+
+            // 3x3 box
+            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value)
+                return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>> &board)
+    {
+        int n = board.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                // check for empty cell
+                if (board[i][j] == '.')
+                {
+                    // try to fill vales [1,9]
+                    for (char val = '1'; val <= '9'; val++)
+                    {
+                        // check for safety
+                        if (isSafe(i, j, board, val))
                         {
-                            // backtrack
-                            board[i][j] = '.';
+                            // insert
+                            board[i][j] = val;
+
+                            // reucusion
+                            bool restSolution = solve(board);
+                            if (restSolution == true)
+                                return true;
+                            else
+                            {
+                                // backtrack
+                                board[i][j] = '.';
+                            }
                         }
                     }
+                    // no valid value from [1,9] , so go back by retuirning false
+                    return false;
                 }
-                // no valid value from [1,9] , so go back by retuirning false
-                return false;
             }
         }
+        // all celss filled
+        return true;
     }
-    // all celss filled
-    return true;
-}
-
-void solveSudoku(vector<vector<char>> &board)
-{
-    solve(board);
-}
-
-int main()
-{
-
-    return 0;
-}
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        solve(board);
+    }
+};
