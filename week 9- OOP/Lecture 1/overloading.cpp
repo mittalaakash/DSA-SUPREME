c562071258780809ac9dd5572e28542ab726e5a7
Mridul <mit.ash.619@gmail.com>
2023-04-02 00:58:36 +0530

    overloading

---
+++ week 9- OOP/Lecture 1/overloading.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Param
+{
+private:
+    /* data */
+public:
+    Param(/* args */);
+    ~Param();
+};
+
+Param::Param(/* args */)
+{
+}
+
+Param::~Param()
+{
+}
+
+int main()
+{
+
+    return 0;
+}
