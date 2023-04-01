3acd3cc5c9083d92f0fdd0012dde86909b0ecd03
Mridul <mit.ash.619@gmail.com>
2023-04-02 00:58:22 +0530

    encapsulation

---
+++ week 9- OOP/Lecture 1/encapsulation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Animal
+{
+private: // using private to make it perfect encapsulation
+    int age;
+    int weight;
+
+    void eat()
+    {
+        cout << "Eating" << endl;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
