9e4724bedd9c6b46e8d3e20f3108797b872cea83
Mridul <mit.ash.619@gmail.com>
2023-04-03 02:14:14 +0530

    overloading update

--- week 9- OOP/Lecture 1/overloading.cpp
+++ week 9- OOP/Lecture 1/overloading.cpp
@@ -4,23 +4,25 @@ using namespace std;
 
 class Param
 {
-private:
-    /* data */
 public:
-    Param(/* args */);
-    ~Param();
+    int val;
+
+    void operator+(Param &obj2)
+    {
+        int value1 = this->val;
+        int value2 = obj2.val;
+
+        cout << (value1 - value2) << endl;
+    }
 };
-
-Param::Param(/* args */)
-{
-}
-
-Param::~Param()
-{
-}
 
 int main()
 {
+    Param obj1, obj2;
+    obj1.val = 7;
+    obj2.val = 5;
+
+    obj1 + obj2; // overloaded opertor to show differenc instead of addition
 
     return 0;
 }
