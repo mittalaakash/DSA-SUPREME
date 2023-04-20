07f42f69acb0d11f8df1ed2cc1b648bbf292ff91
Mridul <mit.ash.619@gmail.com>
2023-04-21 04:35:20 +0530

    break loop

--- week 10 - Linked List/Lecture 3/starting of circular LL.cpp
+++ week 10 - Linked List/Lecture 3/starting of circular LL.cpp
@@ -51,6 +51,45 @@ Node *startingPointLoop(Node *&head)
     return slow;
 }
 
+Node *removeLoop(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "LL is empty " << endl;
+        return NULL;
+    }
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        if (slow == fast)
+        {
+            slow = head;
+            break;
+        }
+    }
+
+    Node *prev = fast;
+    while (slow != fast)
+    {
+        prev = fast;
+        slow = slow->next;
+        fast = fast->next;
+    }
+    prev->next = NULL;
+
+    return slow;
+}
+
 void print(Node *&head)
 {
     Node *temp = head;
@@ -87,6 +126,8 @@ int main()
     ninth->next = fourth;
 
     cout << "Starting Point of LOOP is: " << startingPointLoop(first)->data << endl;
+    removeLoop(first); // if we print circular LL without breaking loop, we'll face infinite loop
+    print(first);
 
     return 0;
 }
