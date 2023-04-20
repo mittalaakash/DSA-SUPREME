f0703e0e9e01c2553d19201557d054ccb26fbabe
Mridul <mit.ash.619@gmail.com>
2023-04-21 04:35:26 +0530

    get middle of LL

---
+++ week 10 - Linked List/Lecture 3/get middle of LL.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *getMiddle(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "LL is empty" << endl;
+        return head;
+    }
+    if (head->next == NULL)
+    {
+        // only 1 node in LL
+        return head;
+    }
+
+    // LL have > 1 node
+    Node *slow = head;
+    Node *fast = head->next;
+
+    while (slow != NULL && fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return slow;
+}
+
+int main()
+{
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+    Node *fourth = new Node(40);
+    Node *fifth = new Node(50);
+    Node *sixth = new Node(60);
+    Node *seventh = new Node(70);
+    Node *eighth = new Node(80);
+    Node *ninth = new Node(90);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
+    seventh->next = eighth;
+    eighth->next = ninth;
+    ninth->next = NULL;
+
+    cout << "Middle Node is: " << getMiddle(first)->data << endl;
+
+    return 0;
+}
