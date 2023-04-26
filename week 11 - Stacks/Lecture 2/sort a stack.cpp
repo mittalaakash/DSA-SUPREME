c4fa0767de6afb310ba7c1dd85989c297e1050c3
Mridul <mit.ash.619@gmail.com>
2023-04-27 02:59:55 +0530

    sort stack

---
+++ week 11 - Stacks/Lecture 2/sort a stack.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void insertSorted(stack<int> &s, int target)
+{
+    // base case
+    if (s.empty())
+    {
+        s.push(target);
+        return;
+    }
+
+    if (s.top() >= target)
+    {
+        s.push(target);
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+    insertSorted(s, target);
+
+    // BT
+    s.push(topElement);
+}
+
+void sortStack(stack<int> &s)
+{
+    // base case
+    if (s.empty())
+    {
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+
+    sortStack(s);
+
+    insertSorted(s, topElement);
+}
+
+int main()
+{
+    stack<int> s;
+
+    s.push(7);
+    s.push(11);
+    s.push(3);
+    s.push(5);
+    s.push(9);
+
+    sortStack(s);
+
+    // "Printing"
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
